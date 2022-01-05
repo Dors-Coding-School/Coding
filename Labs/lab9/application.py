a7067ad68d076fc5cc0a012c04acd7fe52427d45
Dors Coding School <93161768+giovannasproenca@users.noreply.github.com>
2022-01-05 19:36:59 -0300

    Create application.py

---
+++ Labs/lab9/application.py
@@ -0,0 +1,30 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        return redirect("/")
+
+    else:
+        # TODO: Display the entries in the database on index.html
+        people = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", people=people)
