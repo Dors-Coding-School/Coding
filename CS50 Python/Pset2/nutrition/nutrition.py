1afae7ddaf3b4e2e2df7367da03e591ae81ee942
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-04-19 21:25:18 -0300

    Create nutrition.py

---
+++ CS50 Python/Pset2/nutrition/nutrition.py
@@ -0,0 +1,33 @@
+# Create dictionary with all fruits and its calories
+fruits = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
+}
+
+# Get user input
+item = input("Item: ")
+
+# Loop through fruits dictionary
+for key in fruits:
+    # Find the fruit asked (remember to user lowercase)
+    if key == item.lower():
+        # Print fruit's calories
+        print("Calories:", fruits[key])
