afad4f84b8866140e553fb9c0c97bf02dd694e67
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-04-29 13:36:53 -0300

    Update taqueria.py

--- CS50 Python/Pset3/taqueria/taqueria.py
+++ CS50 Python/Pset3/taqueria/taqueria.py
@@ -1,24 +1,30 @@
+# Create menu dictionary
 menu = {
-    "baja taco": 4.00,
-    "burrito": 7.50,
-    "bowl": 8.50,
-    "nachos": 11.00,
-    "quesadilla": 8.50,
-    "super burrito": 8.50,
-    "super quesadilla": 9.50,
-    "taco": 3.00,
-    "tortilla salad": 8.00
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
 }
-
-price = 0
-
+# Set total_amount to 0
+total_amount = 0
+# Loop forever
 while True:
     try:
-        item = input("Item: ")
-        if item.lower() in menu:
-            price += float(menu[item.lower()])
+        # Get user input
+        item = input("Item: ").title()
+        # Check if item is already in the dictionary
+        if item in menu:
+            # Add the item price to total_amount
+            total_amount += menu[item]
+            # Print the current total_amount
             print("Total: $", end="")
-            print("{:.2f}".format(round(price,2)))
+            print("{:.2f}".format(total_amount))
     except EOFError:
+        # Print a new line and stop the loop
         print()
         break
