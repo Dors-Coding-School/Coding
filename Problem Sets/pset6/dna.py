262fb68d92f7a980ebab6637833bfc7114835ab4
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-05-13 21:32:37 -0300

    Update dna.py

--- Problem Sets/pset6/dna.py
+++ Problem Sets/pset6/dna.py
@@ -1,65 +1,81 @@
 import csv
-from sys import argv
+import sys
 
 
 def main():
 
-    # check for correct number of arguments
-    # if correct, we assume we have .csv file in second argument and .txt file in third argument
-    if len(argv) != 3:
-        print("Usage: python dna.py data.csv sequence.txt")
+    # TODO: Check for command-line usage
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python dna.py databases/FILENAME.csv sequences/FILENAME.txt")
 
-    # open files
-    database_file = open("./"+ argv[1])
-    dna_file = open("./" + argv[2])
+    # TODO: Read database file into a variable
+    database = []
+    with open(sys.argv[1], "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        subsequences = reader.fieldnames[1:]
+        for row in reader:
+            database.append(row)
 
-    # obtain STRs from header of database
-    database_reader = csv.DictReader(database_file)
-    strs = database_reader.fieldnames[1:]
+    # TODO: Read DNA sequence file into a variable
+    txtfile = open(sys.argv[2], "r")
+    dna = txtfile.read()
 
+    # TODO: Find longest match of each STR in DNA sequence
+    strs_dna = {}
+    for subsequence in subsequences:
+        longest_run = longest_match(dna, subsequence)
+        strs_dna[subsequence] = longest_run
 
-
-    # copy contents of dna_file into string dna and close file
-    dna = dna_file.read()
-    dna_file.close()
-
-    # count number of occurences of each STR in sequence.txt and store value in dict dna_fingerprint
-    dna_fingerprint = {}
-    for str in strs:
-        dna_fingerprint[str] = consec_repeats(str, dna)
-        
-
-
-    # search through csv file to find match
-    for row in database_reader:
-
-        # if there is match, print name, close files, and end program
-        if match(strs, dna_fingerprint, row):
-            print(f"{row['name']}")
-            database_file.close()
+    # TODO: Check database for matching profiles
+    for person in database:
+        if match(subsequences, strs_dna, person):
+            print(person['name'])
             return
-
-    # if no match was found, print No match and close files.
     print("No match")
-    database_file.close()
+    return
 
-
-# repeats determines the maximum number of consecutive repeats of str in dna
-def consec_repeats(str, dna):
-    i = 0
-    while str*(i+1) in dna:
-        i += 1
-    return i
-
-
-
-
-# match determines whether dna_fingerprint matches row from database
-def match(strs, dna_fingerprint, row):
-    for str in strs:
-        if dna_fingerprint[str] != int(row[str]):
+def match(subsequences, strs_dna, person):
+    for subsequence in subsequences:
+        if strs_dna[subsequence] != int(person[subsequence]):
             return False
     return True
+
+def longest_match(sequence, subsequence):
+    """Returns length of longest run of subsequence in sequence."""
+
+    # Initialize variables
+    longest_run = 0
+    subsequence_length = len(subsequence)
+    sequence_length = len(sequence)
+
+    # Check each character in sequence for most consecutive runs of subsequence
+    for i in range(sequence_length):
+
+        # Initialize count of consecutive runs
+        count = 0
+
+        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
+        # If a match, move substring to next potential match in sequence
+        # Continue moving substring and checking for matches until out of consecutive matches
+        while True:
+
+            # Adjust substring start and end
+            start = i + count * subsequence_length
+            end = start + subsequence_length
+
+            # If there is a match in the substring
+            if sequence[start:end] == subsequence:
+                count += 1
+
+            # If there is no match in the substring
+            else:
+                break
+
+        # Update most consecutive matches found
+        longest_run = max(longest_run, count)
+
+    # After checking for runs at each character in seqeuence, return longest run found
+    return longest_run
 
 
 main()
