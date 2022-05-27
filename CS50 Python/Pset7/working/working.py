5cfd3634a73e917b0c1a5a10411ad572ca9191fe
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-05-27 19:59:10 -0300

    Create working.py

---
+++ CS50 Python/Pset7/working/working.py
@@ -0,0 +1,36 @@
+import re
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    isCorrectFormat = re.search(r"^([0-9][0-2]*:*([0-5][0-9])*) ([A-P]M) to ([0-9][0-2]*:*([0-5][0-9])*) ([A-P]M)$", s)
+    if isCorrectFormat:
+        matches = re.match(r"^([0-9][0-2]*)(:*[0-5_0-9]*) ([A-P]M) to ([0-9][0-2]*)(:*[0-5_0-9]*) ([A-P]M)$", s)
+        pieces = matches.groups()
+        if int(pieces[0]) > 12 or int(pieces[3]) > 12:
+            raise ValueError
+        # Check if first time is in correct 24-hours format
+        first_time = new_format(pieces[0], pieces[1], pieces[2])
+        # Check if second time is in correct 24-hours format
+        second_time = new_format(pieces[3], pieces[4], pieces[5])
+        return first_time + " to " + second_time
+    else:
+        raise ValueError
+
+
+def new_format(hour, minutes, am_pm):
+    if am_pm == 'PM':
+        new_hour = int(hour) + 12
+    else:
+        new_hour = int(hour)
+    if minutes == "":
+        new_time = f"{new_hour:02}" + ":00"
+    else:
+        new_time = f"{new_hour:02}" + minutes
+    return new_time
+
+
+if __name__ == "__main__":
+    main()
