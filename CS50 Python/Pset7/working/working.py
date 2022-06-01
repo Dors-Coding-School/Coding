7502fae68c673e5bd484de3aa72aefca26098ac4
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-06-01 15:44:49 -0300

    Update working.py

--- CS50 Python/Pset7/working/working.py
+++ CS50 Python/Pset7/working/working.py
@@ -5,16 +5,15 @@ def main():
 
 
 def convert(s):
-    isCorrectFormat = re.search(r"^([0-9][0-2]*:*([0-5][0-9])*) ([A-P]M) to ([0-9][0-2]*:*([0-5][0-9])*) ([A-P]M)$", s)
+    isCorrectFormat = re.search(r"^(([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M) to (([0-9][0-2]*):*([0-5][0-9])*) ([A-P]M)$", s)
     if isCorrectFormat:
-        matches = re.match(r"^([0-9][0-2]*)(:*[0-5_0-9]*) ([A-P]M) to ([0-9][0-2]*)(:*[0-5_0-9]*) ([A-P]M)$", s)
-        pieces = matches.groups()
-        if int(pieces[0]) > 12 or int(pieces[3]) > 12:
+        pieces = isCorrectFormat.groups()
+        if int(pieces[1]) > 12 or int(pieces[5]) > 12:
             raise ValueError
-        # Check if first time is in correct 24-hours format
-        first_time = new_format(pieces[0], pieces[1], pieces[2])
-        # Check if second time is in correct 24-hours format
-        second_time = new_format(pieces[3], pieces[4], pieces[5])
+        # Create first time in correct 24-hours format
+        first_time = new_format(pieces[1], pieces[2], pieces[3])
+        # Create second time in correct 24-hours format
+        second_time = new_format(pieces[5], pieces[6], pieces[7])
         return first_time + " to " + second_time
     else:
         raise ValueError
@@ -31,10 +30,10 @@ def new_format(hour, minutes, am_pm):
             new_hour = 0
         else:
             new_hour = int(hour)
-    if minutes == "":
+    if minutes == None:
         new_time = f"{new_hour:02}" + ":00"
     else:
-        new_time = f"{new_hour:02}" + minutes
+        new_time = f"{new_hour:02}" + ":" + minutes
     return new_time
 
 
