d84579cb08570fb8a4c44f5ff0c97a3306dad860
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-06-01 15:33:31 -0300

    Update watch.py

--- CS50 Python/Pset7/watch/watch.py
+++ CS50 Python/Pset7/watch/watch.py
@@ -5,13 +5,11 @@ def main():
 
 
 def parse(s):
-    iframe = re.search(r"(<iframe)(.)*(></iframe>)",s)
-    if iframe:
-        searched = re.search(r"(http(s)*?://(?:www\.)?youtube\.com/embed/)([a-z_A-Z_0-9_]+)", s)
-        if searched:
-            str = searched.group()
-            matches = re.match(r"^(http(s)*?://(?:www\.)?youtube\.com/embed/)([a-z_A-Z_0-9_]+)$", str)
-            return "https://youtu.be/" + matches.groups()[2]
+    if re.search(r"<iframe(.)*><\/iframe>", s):
+        url_pattern = re.search(r"(http(s)*:\/\/(www\.)*youtube\.com\/embed\/)([a-z_A-Z_0-9]+)", s)
+        if url_pattern:
+            split_url = url_pattern.groups()
+            return "https://youtu.be/" + split_url[3]
 
 
 if __name__ == "__main__":
