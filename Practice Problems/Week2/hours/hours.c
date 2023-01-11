70bcb58c51b0750d8162f4cc2aa7321fdb03876e
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-01-11 19:53:31 -0300

    Create hours.c

---
+++ Practice Problems/Week2/hours/hours.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+float calc_hours(int hours[], int weeks, char output);
+
+int main(void)
+{
+    int weeks = get_int("Number of weeks taking CS50: ");
+    int hours[weeks];
+
+    for (int i = 0; i < weeks; i++)
+    {
+        hours[i] = get_int("Week %i HW Hours: ", i);
+    }
+
+    char output;
+    do
+    {
+        output = toupper(get_char("Enter T for total hours, A for average hours per week: "));
+    }
+    while (output != 'T' && output != 'A');
+
+    printf("%.1f hours\n", calc_hours(hours, weeks, output));
+}
+
+// TODO: complete the calc_hours function
+float calc_hours(int hours[], int weeks, char output)
+{
+    int total = 0;
+    for(int i = 0; i < weeks; i++){
+        total += hours[i];
+    }
+    if(output == 'T'){
+        return (float) total;
+    }
+    return (float) total / weeks;
+}
