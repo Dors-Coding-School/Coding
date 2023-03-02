1719023254a52d4f1fa4e1c082df67fe75e67abf
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-03-02 18:54:13 -0300

    Create mario-more.c

---
+++ Problem Sets/pset1/mario-more.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do{
+        height = get_int("Height: ");
+    }
+    while (height <= 0 || height > 8);
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < height; j++){
+            if(i + j < height - 1){
+                printf(" ");
+            }
+            else{
+                printf("#");
+            }
+        }
+        printf("  ");
+        for(int k = 0; k < i+1; k++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
