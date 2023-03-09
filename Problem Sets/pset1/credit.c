0d0557dccd9721ba83df867b509211fff1d43226
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2023-03-09 18:54:35 -0300

    Update credit.c

--- Problem Sets/pset1/credit.c
+++ Problem Sets/pset1/credit.c
@@ -1,150 +1,112 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 #include <math.h>
 
-int get_num_digits(long);
-int sum_every_other_digit(long);
-int multiplyBy2_AddDigits(long);
-int sum_remaining_digits(long);
-bool is_card_valid(long);
-bool isValidAmex(long, int);
-bool isValidVisa(long, int);
-bool isValidMaster(long, int);
-void printCardFlag(long, int);
+int every_other_digit(long credit_card);
+int multiplyAndSum(int last_digit);
+int number_of_digits(long credit_card);
+bool isValidAmex(long credit_card, int numDigit);
+bool isValidMasterCard(long credit_card, int numDigits);
+bool isValidVisa(long credit_card, int numDigits);
 
 int main(void)
 {
-    long card_num = get_long("What is your credit card number: ?\n");
-    int num_digits = get_num_digits(card_num);
-    bool card_valid = is_card_valid(card_num);
-    if (card_valid)
-    {
-        printCardFlag(card_num, num_digits);
+    long credit_card = get_long("Credit Card: ");
+    int sum_every_other_digit = every_other_digit(credit_card);
+    int numDigits = number_of_digits(credit_card);
+    bool amex = isValidAmex(credit_card, numDigits);
+    bool master = isValidMasterCard(credit_card, numDigits);
+    bool visa = isValidVisa(credit_card, numDigits);
+    if (sum_every_other_digit % 10 != 0){
+        printf("INVALID\n");
+        return 0;
     }
-    else
-    {
-        printf("%s", "INVALID\n");
+    else if(amex == true){
+        printf("AMEX\n");
+    }
+    else if(master == true){
+        printf("MASTERCARD\n");
+    }
+    else if(visa == true){
+        printf("VISA\n");
+    }
+    else{
+        printf("INVALID\n");
+        return 0;
     }
 }
 
-bool isValidAmex(long card_num, int num_digits)
-{
-    int first_two = (card_num / pow(10, 13));
-    if ((num_digits == 15) && (first_two == 34 || first_two == 37))
-    {
+bool isValidAmex(long credit_card, int numDigits){
+    int first_two_digits = credit_card / pow(10, 13);
+    if((numDigits == 15) && (first_two_digits == 34 || first_two_digits == 37)){
         return true;
     }
-    return false;
-}
-
-bool isValidMaster(long card_num, int num_digits)
-{
-    int first_two = (int)(card_num / (pow(10, 14)));
-    if ((num_digits == 16) && (first_two > 50 && first_two < 56))
-    {
-        return true;
-    }
-    return false;
-}
-bool isValidVisa(long card_num, int num_digits)
-{
-
-    if (num_digits == 13)
-    {
-        return ((int)(card_num / pow(10, 12)) == 4);
-    }
-    else if (num_digits == 16)
-    {
-        return ((int)(card_num / pow(10, 15)) == 4);
-    }
-    else
-    {
+    else{
         return false;
     }
 }
 
-void printCardFlag(long card_num, int num_digits)
-{
-    if (isValidMaster(card_num, num_digits))
-    {
-        printf("%s", "MASTERCARD\n");
+bool isValidVisa(long credit_card, int numDigits){
+    if(numDigits == 13){
+        int first_digit = credit_card / pow(10, 12);
+        if(first_digit == 4){
+            return true;
+        }
     }
-    else if (isValidVisa(card_num, num_digits))
-    {
-        printf("%s", "VISA\n");
+    else if(numDigits == 16){
+        int first_digit = credit_card / pow(10, 15);
+        if(first_digit == 4){
+            return true;
+        }
     }
-    else if (isValidAmex(card_num, num_digits))
-    {
-        printf("%s", "AMEX\n");
+    return false;
+}
+
+bool isValidMasterCard(long credit_card, int numDigits){
+    int first_two_digits = credit_card / pow(10, 14);
+    if((numDigits == 16) && (first_two_digits > 50 && first_two_digits < 56)){
+        return true;
     }
-    else
-    {
-        printf("%s", "INVALID\n");
+    else{
+        return false;
     }
 }
-bool is_card_valid(long card_num)
-{
-    int num_digits = get_num_digits(card_num);
-    //sum of every other digit multiplied by 2, starting from second-to-last digit
-    int digits_sum1 = sum_every_other_digit(card_num);
-    //sum of number that were not multiplied
-    int digits_sum2 = sum_remaining_digits(card_num);
 
-    int total_sum = digits_sum1 + digits_sum2;
-
-    return ((total_sum % 10) == 0);
-}
-int get_num_digits(long card_num)
-{
+int number_of_digits(long credit_card){
     int count = 0;
-    while (card_num != 0)
-    {
-        card_num /= 10;
-        count += 1;
+    while(credit_card > 0){
+        count = count + 1;
+        credit_card = credit_card / 10;
     }
     return count;
 }
 
-int sum_remaining_digits(long card_num)
-{
-    int isAlternateDigit = true;
+int every_other_digit(long credit_card){
     int sum = 0;
-    while (card_num > 0)
-    {
-        if (isAlternateDigit)
-        {
-            sum += (card_num % 10);
+    bool isAlternateDigit = false;
+    while(credit_card > 0){
+        if(isAlternateDigit == true){
+            int last_digit = credit_card % 10;
+            int product = multiplyAndSum(last_digit);
+            sum = sum + product;
+        }
+        else{
+            int last_digit = credit_card % 10;
+            sum = sum + last_digit;
         }
         isAlternateDigit = !isAlternateDigit;
-        card_num /= 10;
+        credit_card = credit_card / 10;
     }
     return sum;
 }
 
-int sum_every_other_digit(long card_num)
-{
-    int isAlternateDigit = false;
+int multiplyAndSum(int last_digit){
+    int multiply = last_digit * 2;
     int sum = 0;
-    while (card_num > 0)
-    {
-        if (isAlternateDigit)
-        {
-            sum += multiplyBy2_AddDigits(card_num);
-        }
-        isAlternateDigit = !isAlternateDigit;
-        card_num /= 10;
+    while(multiply > 0){
+        int last_digit_multiply = multiply % 10;
+        sum = sum + last_digit_multiply;
+        multiply = multiply / 10;
     }
     return sum;
-}
-
-int multiplyBy2_AddDigits(long card_num)
-{
-    int sumDigits = 0;
-    int current = 2 * (card_num % 10);
-    while (current != 0)
-    {
-        sumDigits += (current % 10);
-        current /= 10;
-    }
-    return sumDigits;
 }
