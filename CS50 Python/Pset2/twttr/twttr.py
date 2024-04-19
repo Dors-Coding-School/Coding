8c1abf247c0476d06546035f19242cea61aa33a5
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2024-04-19 01:15:10 -0300

    Update twttr.py

--- CS50 Python/Pset2/twttr/twttr.py
+++ CS50 Python/Pset2/twttr/twttr.py
@@ -1,11 +1,10 @@
 # Get user input
 answer = input("Input: ")
-# Print "Output: "
-print("Output: ", end="")
+
 # Loop through every letter
 for letter in answer:
     # Check if it is not vowels whether inputted in uppercase or lowercase
     if not letter.lower() in ['a', 'e', 'i', 'o', 'u']:
-        # Print the letter
-        print(letter, end="")
-print()
+        no_vowels += letter
+
+print("Output: " + no_vowels)
