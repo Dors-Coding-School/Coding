9c67840eb48705836a0ea8859b4e490be0d6d6f4
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2024-04-19 01:07:20 -0300

    Update interpreter.py

--- CS50 Python/Pset1/interpreter/interpreter.py
+++ CS50 Python/Pset1/interpreter/interpreter.py
@@ -1,7 +1,10 @@
+# Ask user for input
 math = input("What do you want to know? ")
 
+# Get numbers and operators from input
 x, y, z = math.split(" ")
 
+# Convert strings to float
 new_x, new_z = float(x), float(z)
 
 if y == "+":
@@ -15,4 +18,5 @@ elif y == "/" and z != 0:
 else:
     print("ZeroDivisionError")
 
+# Round the result to only one decimal place
 print(round(result,1))
