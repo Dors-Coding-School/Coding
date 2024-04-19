3fcdd8025d2dd2628fbca22505d5229b4b305dec
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2024-04-19 01:03:54 -0300

    Update bank.py

--- CS50 Python/Pset1/bank/bank.py
+++ CS50 Python/Pset1/bank/bank.py
@@ -1,14 +1,15 @@
 # Get user input
 answer = input("Greeting: ")
 
+# Convert string to lowercase and remove whitespaces
 fix_answer = answer.lower().strip()
 
-# Check if the answer has 'hello', print $0
-if 'hello' in fix_answer:
+# Check if the answer starts with 'hello', print $0
+if fix_answer.startswith("hello"):
     print("$0")
 
 # Check if answer starts with 'h', print $20
-elif fix_answer[0] == 'h':
+elif fix_answer.startswith('h'):
     print("$20")
 
 # Otherwise, print $100
