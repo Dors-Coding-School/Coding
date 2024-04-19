e0a00e26b032c4eb82d125addbedbb2e9c284948
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2024-04-19 00:58:25 -0300

    Update einstein.py

--- CS50 Python/PSET0/einstein/einstein.py
+++ CS50 Python/PSET0/einstein/einstein.py
@@ -1,7 +1,15 @@
-m = int(input("m: "))
+# Ask user for mass
+m = input("m: ")
 
+# Convert mass into an integer
+# Remember that input() always give you a string
+m = int(m)
+
+# Create variable to store constant
 c = 300000000
 
+# Calculate the formula
 E = m * (c ** 2)
 
+# Print the result
 print(E)
