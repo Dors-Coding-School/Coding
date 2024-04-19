7482aae8247d5e8c447ad4b957200e6bc82320bf
giovannasimoespro <98493346+giovannasimoespro@users.noreply.github.com>
2024-04-19 01:11:49 -0300

    Update meal.py

--- CS50 Python/Pset1/meal/meal.py
+++ CS50 Python/Pset1/meal/meal.py
@@ -1,26 +1,30 @@
 def main():
+    # Ask user for time
     time = input("What time is it? ")
 
+    # Call function convert
     converted_time = convert(time)
 
-    # breakfast between 7:00 and 8:00
+    # Breakfast between 7:00 and 8:00
     if (converted_time >= 7 and converted_time <= 8):
         print("breakfast time")
 
-    #  lunch between 12:00 and 13:00
+    # Lunch between 12:00 and 13:00
     if (converted_time >= 12 and converted_time <= 13):
         print("lunch time")
 
-    # dinner between 18:00 and 19:00
+    # Dinner between 18:00 and 19:00
     if (converted_time >= 18 and converted_time <= 19):
         print("dinner time")
 
-
 def convert(time):
+    # Create 2 variables to store hours and minutes
     hours, minutes = time.split(":")
 
+    # Convert minutes into a number between 0 and 1
     converted_minutes = int(minutes) / 60
 
+    # Return the converted value to line 6
     return int(hours) + converted_minutes
 
 
