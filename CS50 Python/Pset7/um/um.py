74f8e4a930db50320d3855e98283486cf520776c
leodalossi <122377761+leodalossi@users.noreply.github.com>
2024-10-09 18:53:06 -0300

    Update um.py

--- CS50 Python/Pset7/um/um.py
+++ CS50 Python/Pset7/um/um.py
@@ -5,7 +5,7 @@ def main():
 
 
 def count(s):
-    um_list = re.findall(r"\b\W*um\W*", s, re.IGNORECASE)
+    um_list = re.findall(r"\bum\b", s, re.IGNORECASE)
     return len(um_list)
 
 if __name__ == "__main__":
