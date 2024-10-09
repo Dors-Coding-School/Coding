58f456856d75e84d47513aa80d4d39961d8cfd9d
leodalossi <122377761+leodalossi@users.noreply.github.com>
2024-10-09 18:31:00 -0300

    Update test_numb3rs.py

--- CS50 Python/Pset7/numb3rs/test_numb3rs.py
+++ CS50 Python/Pset7/numb3rs/test_numb3rs.py
@@ -1,9 +1,5 @@
 from numb3rs import validate
 
-def main():
-    test_format()
-    test_range()
-
 def test_format():
     assert validate(r'cat') == False
     assert validate(r'127') == False
@@ -17,6 +13,3 @@ def test_range():
     assert validate(r'1.512.1.1') == False
     assert validate(r'1.1.512.1') == False
     assert validate(r'1.1.1.512') == False
-
-if __name__ == "__main__":
-    main()
