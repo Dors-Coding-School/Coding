4d1544b717e7bd87252fd638272862229b3203cc
leodalossi <122377761+leodalossi@users.noreply.github.com>
2024-10-09 18:33:20 -0300

    Update test_um.py

--- CS50 Python/Pset7/um/test_um.py
+++ CS50 Python/Pset7/um/test_um.py
@@ -1,11 +1,5 @@
 from um import count
 
-def main():
-    test_upper_lower_case()
-    test_word_with_um()
-    test_sourrounded_by_space()
-
-
 def test_upper_lower_case():
     assert count('Um, thanks for the album') == 1
     assert count('Um, thanks, UM, for, um, the album') == 3
@@ -17,7 +11,3 @@ def test_word_with_um():
 
 def test_sourrounded_by_space():
     assert count('Hi um baby') == 1
-
-
-if __name__ == "__main__":
-    main()
