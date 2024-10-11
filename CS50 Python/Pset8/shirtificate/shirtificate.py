16a22b4d7e4b6ab4b84df4359093e252b69c234a
leodalossi <122377761+leodalossi@users.noreply.github.com>
2024-10-11 18:46:31 -0300

    Update shirtificate.py

--- CS50 Python/Pset8/shirtificate/shirtificate.py
+++ CS50 Python/Pset8/shirtificate/shirtificate.py
@@ -6,20 +6,19 @@ class PDF():
         self._pdf.add_page()
         self._pdf.set_font("helvetica", size=50)
         # Create the Heading
-        self._pdf.cell(w=0, h=60, txt="CS50 Shirtificate",
-                       new_x="LMARGIN", new_y="NEXT", align="C")
+        self._pdf.cell(w=0, h=60, txt="CS50 Shirtificate", ln=1, align="C")
         # Create the Image
-        self._pdf.image("shirtificate.png", w=self._pdf.epw,
-                        alt_text="CS50 Shirtificate")
+        self._pdf.image("shirtificate.png", x=0, y=60, w=self._pdf.epw)
         # Create the Text in the Image
         self._pdf.set_font_size(30)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text(x=47.5, y=140, txt=f"{name} took CS50")
+        # Use positional arguments for the text method
+        self._pdf.text(50, 150, f"{name} took CS50")
     # Save the changes
     def save(self, name):
         self._pdf.output(name)
 
-#Get user's name
+# Get user's name
 name = input("Name: ")
 # Create pdf
 pdf = PDF(name)
