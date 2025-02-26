35e4e29b452740d766cff9cb6cea4f212ca9e575
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:41:20 -0300

    Create 1_return_hello.py

---
+++ CodingDors/1_functions_and_variables/1_return_hello.py
@@ -0,0 +1,17 @@
+# Title: return_hello
+
+"""
+Given a string 's', return a new string with the word "hello" in front. 
+
+return_hello("Gi") -> "hello Gi" 
+
+return_hello("Leo") -> "hello Leo"
+"""
+
+def return_hello(s: str)-> str:
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(return_hello('Gi'))  # Output: 'hello Gi'
+print(return_hello('Leo'))  # Output: 'hello Leo'
+print(return_hello('world'))  # Output: 'hello world'
