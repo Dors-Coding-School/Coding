849cfdfe9189b9153c01af51935b6a2e182de841
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:49:05 -0300

    Create 81_print_to_n_for_loop.py

---
+++ CodingDors/8_loops_in_lists/81_print_to_n_for_loop.py
@@ -0,0 +1,19 @@
+# Title: print_to_n_for_loop
+
+"""
+Write a Python function that takes an integer n as input and returns a list that contains all numbers from 0 to n inclusive, using a for loop.
+ 
+print_to_n_for_loop(5) -> [0, 1, 2, 3, 4, 5]
+
+print_to_n_for_loop(3) -> [0, 1, 2, 3]
+
+print_to_n_for_loop(1) -> [0, 1]
+"""
+
+def print_to_n_for_loop(n: int):
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(print_to_n_for_loop(5))  # Output: [0, 1, 2, 3, 4, 5]
+print(print_to_n_for_loop(3))  # Output: [0, 1, 2, 3]
+print(print_to_n_for_loop(1))  # Output: [0, 1]
