1928f6d10b521f10f2d6b26cfb3160584c7f8669
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:47:46 -0300

    Create 128_count_vowels_str.py

---
+++ CodingDors/7_loops_in_strings/128_count_vowels_str.py
@@ -0,0 +1,19 @@
+# Title: count_vowels_str
+
+"""
+Write a function that counts the number of vowels in a given string.
+
+count_vowels_str('hello') -> 2
+
+count_vowels_str('codingDORS') -> 3
+
+count_vowels_str('APPLEbanana') -> 5
+"""
+
+def count_vowels_str(s: str) -> int:
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(count_vowels_str('hello'))  # Output: 2
+print(count_vowels_str('codingDORS'))  # Output: 3
+print(count_vowels_str('APPLEbanana'))  # Output: 5
