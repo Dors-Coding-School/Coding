50264d75d5a94a96cea2f4b53ab5cd2c24d408c7
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:46:39 -0300

    Create 21_return_quarters.py

---
+++ CodingDors/6_loops/21_return_quarters.py
@@ -0,0 +1,19 @@
+# Title: return_quarters
+
+"""
+Given an integer called 'change', use a while loop to determine how many quarters will be given as 'change'. 
+
+return_quarters(76) -> 3 
+
+return_quarters(29) -> 1
+"""
+
+def return_quarters(change: int) -> int:
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(return_quarters(76))  # Output: 3
+print(return_quarters(29))  # Output: 1
+print(return_quarters(100))  # Output: 4
+print(return_quarters(15))  # Output: 0
+print(return_quarters(-5))  # Output: 0
