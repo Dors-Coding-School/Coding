192004cf67e85cac4a1432b4eed1bc68d7659870
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:45:41 -0300

    Create 257_first_element.py

---
+++ CodingDors/5_lists/257_first_element.py
@@ -0,0 +1,21 @@
+# Title: first_element
+
+"""
+Write a function that returns the first element of a given list.
+
+first_element([1,2,3,4,5]) -> 1
+
+first_element([5,4,3,2,1]) -> 5
+
+first_element([10,20,30,40,50]) -> 10
+"""
+
+def first_element(n: list) -> int:
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(first_element([1, 2, 3, 4, 5]))  # Output: 1
+print(first_element([5, 4, 3, 2, 1]))  # Output: 5
+print(first_element([10, 20, 30, 40, 50]))  # Output: 10
+print(first_element([7, 8, 9]))  # Output: 7
+print(first_element([-1, -2, -3, -4, -5]))  # Output: -1
