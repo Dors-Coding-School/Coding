9a841dedb58f74258666c89d121bd957e304432e
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-02-26 18:43:40 -0300

    Create 101_is_even.py

---
+++ CodingDors/3_conditionals/101_is_even.py
@@ -0,0 +1,19 @@
+# Title: is_even
+
+"""
+Write a function that checks if a given number is even.
+
+is_even(5) -> False
+
+is_even(10) -> True
+
+is_even(99) -> False
+"""
+
+def is_even(n: int) -> bool:
+    return
+
+# Test your code (copy and past this block of code to test your solution)
+print(is_even(5))  # Output: False
+print(is_even(10))  # Output: True
+print(is_even(99))  # Output: False
