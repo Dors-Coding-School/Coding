90ce18c15a83db1c3bfec0455afe4988267f5499
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2025-05-12 18:05:28 -0300

    Update june_vacancies.sql

--- CS50 SQL/week4/bnb/june_vacancies.sql
+++ CS50 SQL/week4/bnb/june_vacancies.sql
@@ -1,5 +1,5 @@
 CREATE VIEW "june_vacancies" AS
 SELECT listings.id, listings.property_type, listings.host_name, COUNT(availabilities.date) AS "days_vacant" FROM listings
 JOIN availabilities ON listings.id = availabilities.listing_id
-WHERE availabilities.date LIKE "2023-06-%"
+WHERE availabilities.date LIKE "2023-06-%" AND availabilities.available = "TRUE"
 GROUP BY listings.id;
